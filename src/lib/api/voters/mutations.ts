aa7ee8172b6305c871a7b2483344c4ddaa7aefb2
Hernan Valencia <hernanadrianv17@gmail.com>
2024-04-27 08:53:48 -0500

    🐛 fix: fixing mutations bug

--- src/lib/api/voters/mutations.ts
+++ src/lib/api/voters/mutations.ts
@@ -1,18 +1,11 @@
 import { db } from "@/lib/db/index";
-import { 
-  VoterId, 
-  NewVoterParams,
-  UpdateVoterParams, 
-  updateVoterSchema,
-  insertVoterSchema, 
-  voterIdSchema 
-} from "@/lib/db/schema/voters";
+import { VoterId, NewVoterParams, UpdateVoterParams, updateVoterSchema, insertVoterSchema, voterIdSchema } from "@/lib/db/schema/voters";
 
 export const createVoter = async (voter: NewVoterParams) => {
   const newVoter = insertVoterSchema.parse(voter);
   try {
     const v = await db.voter.create({ data: newVoter });
-    return { voter: v };
+    return { voter: v, success: true };
   } catch (err) {
     const message = (err as Error).message ?? "Error, please try again";
     console.error(message);
@@ -24,8 +17,8 @@ export const updateVoter = async (id: VoterId, voter: UpdateVoterParams) => {
   const { id: voterId } = voterIdSchema.parse({ id });
   const newVoter = updateVoterSchema.parse(voter);
   try {
-    const v = await db.voter.update({ where: { id: voterId }, data: newVoter})
-    return { voter: v };
+    const v = await db.voter.update({ where: { id: voterId }, data: newVoter });
+    return { voter: v, success: true };
   } catch (err) {
     const message = (err as Error).message ?? "Error, please try again";
     console.error(message);
@@ -36,12 +29,11 @@ export const updateVoter = async (id: VoterId, voter: UpdateVoterParams) => {
 export const deleteVoter = async (id: VoterId) => {
   const { id: voterId } = voterIdSchema.parse({ id });
   try {
-    const v = await db.voter.delete({ where: { id: voterId }})
-    return { voter: v };
+    const v = await db.voter.delete({ where: { id: voterId } });
+    return { voter: v, success: true };
   } catch (err) {
     const message = (err as Error).message ?? "Error, please try again";
     console.error(message);
     throw { error: message };
   }
 };
-
